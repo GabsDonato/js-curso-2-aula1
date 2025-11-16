fe37fb1024a4452b91e43d3c4f1f9deb6ccea69e
Gabriela Donato <gabi.donato726@gmail.com>
2025-11-16 13:40:52 -0500

    Add initial game setup and verification function

---
+++ app.js
@@ -0,0 +1,10 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Jogo do numero secreto';
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Descubra o número secreto entre 1 e 10!';
+
+function verificarChute()  {
+    console.log('Função verificarChute foi chamada');
+
+}
