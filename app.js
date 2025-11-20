ad7eb5ab59ff4796f291817082f12f319edc7b19
Gabriela Donato <gabi.donato726@gmail.com>
2025-11-19 21:25:07 -0500

    Update verification function to compare user input with the secret number

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ exibirTextoNaTela('p', 'Escolha um número entre 1 e 10!');
 
 
 function verificarChute()  {
-    console.log(numeroSecreto);
+    let chute = document.querySelector('input').value;
+    console.log(chute == numeroSecreto);
 
 }
 
