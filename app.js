a7ff34e0ed23d47999465cd1241f1a3abab7bbd9
Gabriela Donato <gabi.donato726@gmail.com>
2025-11-19 21:08:55 -0500

    Refactor game text display and cleanup commented code

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do numero secreto';
+// let titulo = document.querySelector('h1');
+// titulo.innerHTML = 'Jogo do numero secreto';
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Descubra o número secreto entre 1 e 10!';
+// let paragrafo = document.querySelector('p');
+// paragrafo.innerHTML = 'Descubra o número secreto entre 1 e 10!';
+
+let numeroSecreto = gerarNumeroAleatorio();
+
+function exibirTextoNaTela(tag , texto){
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+exibirTextoNaTela('h1', 'Jogo do número secreto');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10!');
+
 
 function verificarChute()  {
-    console.log('Função verificarChute foi chamada');
+    console.log(numeroSecreto);
 
+}
+
+function gerarNumeroAleatorio(){
+    return parseInt(Math.random() * 10 + 1);
 }
